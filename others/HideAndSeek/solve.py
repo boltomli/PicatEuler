64038212242e2003010d246692c93c925a36381e
Song Li <songl@outlook.com>
2016-12-07 17:18:14 +0800

    Solve and Visualize Hide&Seek

--- others/HideAndSeek/solve.py
+++ others/HideAndSeek/solve.py
@@ -2,7 +2,7 @@
 
 # -*- coding: utf-8 -*-
 
-from itertools import permutations, chain
+from itertools import chain, product
 
 # Board
 
@@ -19,7 +19,7 @@ Rhino = 11
 # -------------
 # R E Z |
 # G   G |   R L
-# L   R | G E Z 
+# L   R | G E Z
 
 board = [Elephant, Empty, Lion, Zebra, Gazelle, Zebra, Rhino, Lion, Elephant,
          Gazelle, Empty, Lion, Empty, Zebra, Elephant, Lion, Rhino, Gazelle,
@@ -49,21 +49,35 @@ def rotate(mask):
             mask[1], mask[4], mask[7],
             mask[0], mask[3], mask[6]]
 
-basic_masks = [
-               [
-                [0, 0, 1, 1, 0, 0, 1, 0, 0]
-               ],
-               [
-                [0, 0, 0, 1, 0, 1, 0, 0, 0]
-               ],
-               [
-                [0, 1, 0, 0, 1, 0, 0, 0, 0]
-               ],
-               [
-                [0, 0, 1, 0, 0, 0, 0, 1, 0]
-               ]
-              ]
+def visualize(masks):
+    assert len(masks) == 3 * 3 * 4
+    mask = ''
+    for i in range(0, len(masks)):
+        if i % 3 == 0:
+            mask += '\n'
+        if i % 9 == 0:
+            mask += '------\n'
+        if masks[i] == 0:
+            mask += '██'
+        else:
+            mask += '  '
+    mask += '\n------'
+    print(mask)
 
+basic_masks = [
+                  [
+                      [0, 0, 1, 1, 0, 0, 1, 0, 0]
+                  ],
+                  [
+                      [0, 0, 0, 1, 0, 1, 0, 0, 0]
+                  ],
+                  [
+                      [0, 1, 0, 0, 1, 0, 0, 0, 0]
+                  ],
+                  [
+                      [0, 0, 1, 0, 0, 0, 0, 1, 0]
+                  ]
+              ]
 
 length = len(basic_masks)
 
@@ -73,19 +87,15 @@ for i in range(0, length):
         basic_masks[i].append(rotated_mask)
         rotated_mask = rotate(rotated_mask)
 
-mask_group_permutations = list(permutations(basic_masks))
-mask_permutations = []
-for mask_group in mask_group_permutations:
-    for masks in mask_group:
-        mask_permutations.append(masks)
+all_permutations = list(chain(product(basic_masks[0], basic_masks[1], basic_masks[2], basic_masks[3])))
 
-target = Gazelle ** 5
+target = Lion * Zebra * Rhino * Elephant ** 3
 
-for m in mask_permutations:
+for m in all_permutations:
     masks = list(chain.from_iterable(m))
-    if len(masks) == len(board):
-        result = 1
-        for i in range(0, len(board)):
-            result *= board[i] ** masks[i]
-        if result == target:
-            print(masks)
+    assert len(masks) == len(board)
+    result = 1
+    for i in range(0, len(board)):
+        result *= board[i] ** masks[i]
+    if result == target:
+        visualize(masks)
